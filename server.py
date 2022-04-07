d1b2f653212bc520b8b06eac689e5dda193b02fe
Samuel Miller <smillercb@gmail.com>
2022-04-07 17:46:28 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def draw():
+    return render_template("index.html",num1=8,num2=8)
+
+@app.route('/<int:num1>')
+def draw2(num1):
+    return render_template("index.html",num1=num1,num2=8)
+
+@app.route('/<int:num1>/<int:num2>')
+def draw3(num1,num2):
+    return render_template("index.html",num1=num1,num2=num2)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
